26f02718bbba6295db5e86401650d79bc095055c
Code Nithin <code.nithin22@gmail.com>
2023-10-03 21:16:36 -0400

    DJ Database URL removed

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -196,13 +196,6 @@ REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 REDIS_DB = 0
 
-import dj_database_url
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(
-        conn_max_age=500,
-        conn_health_checks=True,
-    )
 
 # Static file serving.
 # https://whitenoise.readthedocs.io/en/stable/django.html#add-compression-and-caching-support
